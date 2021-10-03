d243997cf6eea4a688fd2801e11de52704b0f782
Kartik <kartikvishnoi778@gmail.com>
2021-10-03 23:27:05 +0530

    Added code to fid kth min and max element in array

---
+++ Arrays/kth_max_min.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+void array_max()
+{
+    int n,k,temp=0;
+    std::cout<<"Enter no. of elements:  ";
+    std::cin>>n;
+
+    int arr[n];
+    std::cout<<"Enter elements:  ";
+    for(int i=0;i<n;i++)
+        std::cin>>arr[i];
+    
+    std::cout<<"\nEnter k:  ";
+    std::cin>>k;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    std::cout<<"\nK min element:  "<<arr[k-1];
+    std::cout<<"\nK max element:  "<<arr[n-k+1];
+}
+
+int main()
+{
+    array_max();
+    return 0;
+}
