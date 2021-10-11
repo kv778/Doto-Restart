629947e1855014a93e676c799a22e478e8e558c8
Kartik <kartikvishnoi778@gmail.com>
2021-10-12 01:31:14 +0530

    Added code to sort a vector of 0s,1s and 2s only

--- Arrays/012_sort.cpp
+++ Arrays/012_sort.cpp
@@ -42,8 +42,52 @@ void sort_array()
         std::cout<<sorted_arr[i]<<"  ";
 }
 
+void sort_vector()
+{
+    std::vector<int> vec;
+    int temp=0;
+
+    std::cout<<"Enter elements of vector, 99 to stop:  ";
+    while(temp!=99)
+    {
+        std::cin>>temp;
+        vec.push_back(temp);
+    }
+    vec.pop_back();
+
+    int ctr=0;
+    auto it = vec.begin();
+
+    for(int i=0;i<vec.size();i++)
+    {
+        if(vec[i] == 0)
+        {
+            vec.erase(it+i);
+            vec.insert(it+ctr, 0);
+            ++ctr;
+        }
+    }
+
+    for(int i=0;i<vec.size();i++)
+    {
+        if(vec[i] == 1)
+        {
+            vec.erase(it+i);
+            vec.insert(it+ctr, 1);
+            ++ctr;
+        }
+    }
+
+    std::cout<<"\n Sorted vector:  ";
+    for(int i=0;i<vec.size();i++)
+    {
+        std::cout<<vec[i]<<"  ";
+    }
+}
+
 int main()
 {
-    sort_array();
+    //sort_array();
+    sort_vector();
     return 0;
 }
